4021f2251a30e0b9f5005dcda93bb566c25e5bdf
Gerardo <technicalzfg@gmail.com>
2025-02-04 14:48:20 -0400

    Initial project

---
+++ src/views/HistorialView.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="mt-[87px] mx-auto max-w-[330px]">
+    <h1 class="text-[45px]">UP3</h1>
+
+    <div
+      class="relative mt-[27px] border border-dotted border-black w-[205px] h-[39px] rounded-[10px] text-[10px] flex flex-col items-start justify-center pl-[12px]"
+    >
+      <span class="block">Puma Store CCS</span>
+      <span>Descripción: Cajero 1</span>
+      <div class="absolute right-0 left-0 bottom-[-66px] flex justify-center">
+        <img src="../assets/img/arrow-up-down.png" alt="" />
+      </div>
+    </div>
+
+    <div
+      class="mt-[65px] border border-black rounded-[20px] h-[375px] max-h-[375px] py-[24px] px-[27px]"
+    >
+      <div class="flex flex-col">
+        <div class="flex justify-between items-center">
+          <div class="flex flex-col">
+            <span class="text-xs font-bold">$20,000 USDT</span>
+            <span class="text-[9px]">6.578769 NEAR - 2024-10-10 / 20:18</span>
+            <span class="text-[9px]">Nota: 5 cervezas y 1 pastel</span>
+          </div>
+
+          <div class="flex items-center gap-2.5">
+            <img src="../assets/img/eclipse-full.png" alt="" />
+            <span class="text-[9px]">Completado</span>
+          </div>
+        </div>
+        <div class="border-t border-dotted border-black my-2"></div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+</script>
+
+<template>
+  <div class="max-w-[330px] mx-auto mt-[200px]">
+    <div class="relative border-[1px] border-black rounded-[20px] h-[394px]">
+      <div class="absolute left-0 right-0 top-[-20px] flex justify-center">
+        <div
+          class="bg-white border-[1px] border-dotted border-black max-w-max py-[13px] px-[24px] rounded-[20px] inline-flex items-center gap-2.5"
+        >
+          <img src="../assets/img/asterisco.png" alt="" />
+          <span class="text-[9px] leading-none">
+            Requisito obligatorio para todos los usuarios
+          </span>
+        </div>
+      </div>
+
+      <div class="relative mt-[45px] flex justify-center items-center">
+        <img src="../assets/img/border-rounded-pounts.png" alt="" />
+        <img src="../assets/img/qr.png" class="absolute" alt="" />
+      </div>
+
+      <div
+        class="border-[1px] border-black mt-[20px] mx-[26px] py-[10px] px-[14px] rounded-[13px] flex items-center"
+      >
+        <input
+          type="text"
+          placeholder="Pegar código de punto de venta"
+          class="text-[12px] w-full"
+        />
+        <button>
+          <img src="../assets/img/paste.png" alt="" />
+        </button>
+      </div>
+
+      <div class="absolute left-0 right-0 bottom-[-24px] flex justify-center">
+        <v-btn
+          flat
+          color="#662483"
+          class="text-white py-[12px] !rounded-[13px] text-sm w-[160px] min-h-[39px] tracking-normal !normal-case !font-normal"
+          @click="router.push('/historial')"
+        >
+          Iniciar
+        </v-btn>
+      </div>
+    </div>
+    <div class="mt-[130px] flex justify-between items-center">
+      <span class="text-sm">
+        Escanea para <br />
+        conectar la <br />
+        aplicación
+      </span>
+
+      <div class="inline-flex flex-col items-end">
+        <img src="../assets/img/dots.png" alt="" />
+        <h1 class="text-[64px]">UP3</h1>
+      </div>
+    </div>
+  </div>
+</template>
